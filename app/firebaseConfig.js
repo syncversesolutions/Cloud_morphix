e331a72edaf32f89e17711813409fe00c01f8e17
syncversesolutions <syncversesolutions@gmail.com>
2025-06-17 15:30:00 +0530

    2nd push, user authentication is done, data adding into DB

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 
@@ -21,7 +20,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 export {db};
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-' use client'
+'use client';
 import { db } from './firebaseConfig'
-import { collection, addDoc } from 'firebase/firestore/lite';
-import React from 'react';
+import { collection, addDoc } from 'firebase/firestore';
+import React, { useState } from 'react';
 //import Image from "next/image";
 //import styles from "./page.module.css";
 
@@ -24,11 +24,76 @@ async function addDataToFireStore(name: any, email: any, message: any) {
 
 
 export default function Home() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    const added = await addDataToFireStore(name, email, message);
+    if (added) {
+      setName("");
+      setEmail("");
+      setMessage("");
+
+      alert("Data added to Firestore DB!!")
+    }
+  };
    return (
     <><main className='flex min-h-screen flex-col items-center p-24'>
     <h1 className="text-5xl font-bold m-10">
        Add Data to Firebase
      </h1>
+     <form onSubmit={handleSubmit} className='max-w-md mx-auto p-4 bg-white shadow-md'>
+     <div className='mb-4'>
+      <label htmlFor='name' className='block text-gray-700 font-bold mb-2'>
+        Name:
+      </label>
+      <input
+      type='text'
+      id='name'
+      className='w-full px-3 py-2 border rounded-lg focus:outline none focus:border-blue-500'
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+      />
+     </div>
+
+     <div className='mb-4'>
+      <label htmlFor='email' className='block text-gray-700 font-bold mb-2'>
+        Email:
+      </label>
+      <input
+      type='text'
+      id='email'
+      className='w-full px-3 py-2 border rounded-lg focus:outline none focus:border-blue-500'
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      />
+     </div>
+
+     <div className='mb-4'>
+      <label htmlFor='message' className='block text-gray-700 font-bold mb-2'>
+        Message:
+      </label>
+      <textarea
+      rows={5}
+      id='message'
+      className='w-full px-3 py-2 border rounded-lg focus:outline none focus:border-blue-500'
+      value={message}
+      onChange={(e) => setMessage(e.target.value)}
+      ></textarea>
+     </div>
+
+     <div>
+      <div className='text-center'>
+       <button type='submit' className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg'>
+  Submit
+</button>
+
+      </div>
+      Submit
+     </div>
+     </form>
      </main></>
   )
 }
